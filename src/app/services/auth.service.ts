c65984f57e13d1fb775323c2e463e920bba46cbf
Anuar7777 <aman_anuar@mail.ru>
2025-07-26 20:28:08 +0500

    feat: add core service modules
    - auth-token.service
    - auth.service
    - user.service
    - pomodoro.service
    - task.service
    - time-block.service

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,46 @@
+import { IAuthForm, IAuthResponse } from '@/types/auth.types'
+
+import { axiosClassic } from '@/api/interceptors'
+
+import { removeFromStorage, saveTokenStorage } from './auth-token.service'
+
+class AuthService {
+	private BASE_URL = `/auth`
+
+	async main(type: 'login' | 'register', data: IAuthForm) {
+		const response = await axiosClassic.post<IAuthResponse>(
+			`${this.BASE_URL}/${type}`,
+			data
+		)
+
+		if (response.data.accessToken) {
+			saveTokenStorage(response.data.accessToken)
+		}
+
+		return response
+	}
+
+	async getNewTokens() {
+		const response = await axiosClassic.post<IAuthResponse>(
+			`${this.BASE_URL}/refresh`
+		)
+
+		if (response.data.accessToken) {
+			saveTokenStorage(response.data.accessToken)
+		}
+
+		return response
+	}
+
+	async logout() {
+		const response = await axiosClassic.post<boolean>(`${this.BASE_URL}/logout`)
+
+		if (response.data) {
+			removeFromStorage()
+		}
+
+		return response
+	}
+}
+
+export const authService = new AuthService()
